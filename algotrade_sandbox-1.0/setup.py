abbfc47306ebdc1d64a7aa0ade579e02467e1f0c
DavC95 <32800312+DavC95@users.noreply.github.com>
2017-10-15 17:11:10 +0200

    Update setup.py
    Edited dependencies

--- algotrade_sandbox-1.0/setup.py
+++ algotrade_sandbox-1.0/setup.py
@@ -3,4 +3,5 @@ from distutils.core import setup
 setup(name='algotrade_sandbox',
       version='1.0',
       py_modules=['algotrade_sandbox'],
+      install_requires=['numpy','pandas','matplotlib']
       )
